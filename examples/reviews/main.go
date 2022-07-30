8ea7b90b7b27dc731438cbf292e42c0186d2003c
Marina Valverde <mdval.eh@gmail.com>
2022-07-29 23:48:15 -0300

    reviews: Update example

--- examples/reviews/main.go
+++ examples/reviews/main.go
@@ -11,7 +11,7 @@ type example struct {
 }
 
 func (e example) GetReview() {
-	credit, _, err := e.client.Reviews.GetReview("5e7d573b6c849200126ac199")
+	credit, _, err := e.client.Reviews.GetReview("62e08236a44d0907dc16bce4")
 	if err != nil {
 		panic(errors.Wrap(err, "failed to get review"))
 	}
