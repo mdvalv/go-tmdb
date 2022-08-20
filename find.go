d985c5f10c602066cb53536ff722ce986b012eff
Marina Valverde <mdval.eh@gmail.com>
2022-08-20 18:42:49 -0300

    find: Refact structs

--- find.go
+++ find.go
@@ -12,19 +12,45 @@ type FindResource struct {
 	client *Client
 }
 
+type EpisodeFinding struct {
+	AirDate        string  `json:"air_date"`
+	EpisodeNumber  int     `json:"episode_number"`
+	Id             int     `json:"id"`
+	MediaType      string  `json:"media_type"`
+	Name           string  `json:"name"`
+	Overview       string  `json:"overview"`
+	ProductionCode string  `json:"production_code"`
+	Runtime        int     `json:"runtime"`
+	SeasonNumber   int     `json:"season_number"`
+	ShowId         int     `json:"show_id"`
+	StillPath      *string `json:"still_path"`
+	VoteAverage    float64 `json:"vote_average"`
+	VoteCount      int     `json:"vote_count"`
+}
+
+type SeasonFinding struct {
+	AirDate      string  `json:"air_date"`
+	EpisodeCount int     `json:"episode_count"`
+	Id           int     `json:"id"`
+	MediaType    string  `json:"media_type"`
+	Name         string  `json:"name"`
+	Overview     string  `json:"overview"`
+	PosterPath   *string `json:"poster_path"`
+	SeasonNumber int     `json:"season_number"`
+}
+
 type Findings struct {
-	MovieResults     []Movie   `json:"movie_results"`
-	PersonResults    []Person  `json:"person_results"`
-	TvResults        []TVShow  `json:"tv_results"`
-	TvEpisodeResults []Episode `json:"tv_episode_results"`
-	TvSeasonResults  []Season  `json:"tv_season_results"`
+	MovieResults     []Movie          `json:"movie_results"`
+	PersonResults    []Person         `json:"person_results"`
+	TvResults        []TVShow         `json:"tv_results"`
+	TvEpisodeResults []EpisodeFinding `json:"tv_episode_results"`
+	TvSeasonResults  []SeasonFinding  `json:"tv_season_results"`
 }
 
 type FindOptions languageOptions
 
-// The find method makes it easy to search for objects in our database by an external id.
+// The find method makes it easy to search for objects by an external id.
 // This method will search all objects (movies, TV shows and people) and return the results in a single response.
-// The supported external sources for each object are as follows.
 // Allowed values for external source:
 //    imdb_id, freebase_mid, freebase_id, tvdb_id, tvrage_id, facebook_id, twitter_id, instagram_id
 func (fr *FindResource) Find(externalId, externalSource string, opt *FindOptions) (*Findings, *http.Response, error) {
