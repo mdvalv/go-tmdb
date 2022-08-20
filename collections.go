7c12192fab6ddb0334df442ec3b98b6b9fde9dc1
Marina Valverde <mdval.eh@gmail.com>
2022-08-19 23:56:33 -0300

    collections: Refact structs

--- collections.go
+++ collections.go
@@ -38,14 +38,7 @@ type Collection struct {
 	Parts        []Part  `json:"parts"`
 }
 
-type CollectionsOptions struct {
-	// Pass a ISO 639-1 value to display translated data for the fields that support it.
-	// minLength: 2
-	// pattern: ([a-z]{2})-([A-Z]{2})
-	// default: en-US
-	// If the provided language is wrong, it is ignored.
-	Language string `url:"language,omitempty" json:"language,omitempty"`
-}
+type CollectionsOptions languageOptions
 
 // Get collection details by id.
 func (cr *CollectionsResource) GetCollection(id int, opt *CollectionsOptions) (*Collection, *http.Response, error) {
